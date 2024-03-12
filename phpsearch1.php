8a85c516a12be396c2a7946864bc1e198ef6b133
ajayprajapati09 <140874138+ajayprajapati09@users.noreply.github.com>
2024-03-12 10:03:14 +0530

    Update phpsearch1.php

--- phpsearch1.php
+++ phpsearch1.php
@@ -121,12 +121,12 @@
     </nav>
     <!-- Navbar End -->
     <!--search process-->
-    <div>
+<!--     <div>
       <form class="d-flex" role="search" action="phpsearch1.php" method="get">
         <input class="inpbox" type="text" name="name" placeholder="Search" aria-label="Search">
         <button class="srh" type="submit">Search</button>
       </form>
-    </div>
+    </div> -->
     <!--search end-->
     <!-- Page Header Start -->
     <div
