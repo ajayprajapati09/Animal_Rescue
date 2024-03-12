e09f96f0f61cbe7e571e0ec371b402609e8e94ea
ajayprajapati9802@gmail.com <ajayprajapati9802@gmail.com>
2024-03-12 08:42:22 +0530

    first commit

---
+++ phpsearch.php
@@ -0,0 +1,4 @@
+<form class="d-flex" role="search" action="phpsearch1.php" method="get">
+                    <input class="inpbox" type="text" name="name" placeholder="Search" aria-label="Search">
+                    <button class="srh" type="submit">Search</button>
+                  </form>
